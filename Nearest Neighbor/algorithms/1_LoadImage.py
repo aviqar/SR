4bc97efe65e47853be8b0edef6fc4168bed4a4f7
GPN Lab <gpn@megaloberries.local>
2016-02-09 15:49:09 -0500

    fixed image load

--- Nearest Neighbor/algorithms/1_LoadImage.py
+++ Nearest Neighbor/algorithms/1_LoadImage.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  9 15:06:02 2016
+import scipy.io
+import numpy as np
 
-@author: gpn
-"""
+data = scipy.io.loadmat('/Volumes/GPN2/SR/HBI_350021_swinorm_33.mat')
+newbrain = data['img']
+newbrain = newbrain[:,128:1024-128]
 
+np.save('/Volumes/GPN2/SR/Nearest Neighbor/data/originalImage.npy', newbrain)
